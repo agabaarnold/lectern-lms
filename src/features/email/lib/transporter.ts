166518daa42356bf3df2ff5f3b2ceddb9e3fb18d
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-18 17:24:15 +0300

    feat: add email transporter configuration using nodemailer

---
+++ src/features/email/lib/transporter.ts
@@ -0,0 +1,15 @@
+import { createTransport } from "nodemailer";
+
+import { env } from "#/env.ts";
+
+const transporter = createTransport({
+	host: env.SMTP_HOST,
+	port: env.SMTP_PORT,
+	secure: env.SMTP_PORT === 465,
+	auth: {
+		user: env.SMTP_USER,
+		pass: env.SMTP_PASS,
+	},
+});
+
+export { transporter };
