dc73600ca49083debb05e9170dd77774efc5a62a
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-18 19:37:27 +0300

    refactor: simplify database initialization by removing unnecessary function wrapper

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,13 @@
 import { neon } from "@neondatabase/serverless";
-import { createServerOnlyFn } from "@tanstack/react-start";
 import { drizzle } from "drizzle-orm/neon-http";
 
 import { env } from "#/env.ts";
 
 import { authRelations, mainRelations } from "./relations";
 
-export const db = createServerOnlyFn(() => {
-	const dbUrl = env.DATABASE_URL;
-	const sql = neon(dbUrl);
+const sql = neon(env.DATABASE_URL);
 
-	return drizzle({
-		client: sql,
-		relations: { ...mainRelations, ...authRelations },
-	});
+export const db = drizzle({
+	client: sql,
+	relations: { ...mainRelations, ...authRelations },
 });
