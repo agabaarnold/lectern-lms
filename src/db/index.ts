ca010fbfbae5e3f282898b2b148b3b7343f7c093
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-19 19:12:01 +0300

    refactor(db): simplify relations import and usage in drizzle configuration

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,11 +3,11 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import { env } from "#/env.server.ts";
 
-import { authRelations, mainRelations } from "./relations";
+import { relations } from "./relations";
 
 const sql = neon(env.DATABASE_URL);
 
 export const db = drizzle({
 	client: sql,
-	relations: { ...mainRelations, ...authRelations },
+	relations,
 });
