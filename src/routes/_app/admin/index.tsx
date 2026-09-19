093c8fd1395691bd8b726be50056bcfffce2d269
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-19 18:20:38 +0300

    refactor: simplify AdminDashboard layout by removing Sidebar components

--- src/routes/_app/admin/index.tsx
+++ src/routes/_app/admin/index.tsx
@@ -1,13 +1,10 @@
-// oxlint-disable react/function-component-definition func-style anti-slop/require-safety-comment-for-type-assertion
+// oxlint-disable react/function-component-definition func-style
 import { createFileRoute } from "@tanstack/react-router";
 
 import data from "#/app/dashboard/data.json";
-import { AppSidebar } from "#/components/sidebar/app-sidebar.tsx";
 import { ChartAreaInteractive } from "#/components/sidebar/chart-area-interactive.tsx";
 import { DataTable } from "#/components/sidebar/data-table.tsx";
 import { SectionCards } from "#/components/sidebar/section-cards.tsx";
-import { SiteHeader } from "#/components/sidebar/site-header.tsx";
-import { SidebarInset, SidebarProvider } from "#/components/ui/sidebar.tsx";
 
 export const Route = createFileRoute("/_app/admin/")({
 	component: AdminDashboard,
@@ -15,32 +12,14 @@ export const Route = createFileRoute("/_app/admin/")({
 
 function AdminDashboard() {
 	return (
-		<SidebarProvider
-			style={
-				{
-					"--sidebar-width": "calc(var(--spacing) * 72)",
-					"--header-height": "calc(var(--spacing) * 12)",
-				} as React.CSSProperties
-			}
-		>
-			<AppSidebar variant="inset" />
-			<SidebarInset>
-				<SiteHeader />
+		<>
+			<SectionCards />
 
-				<div className="flex flex-1 flex-col">
-					<div className="@container/main flex flex-1 flex-col gap-2">
-						<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-							<SectionCards />
+			<div className="px-4 lg:px-6">
+				<ChartAreaInteractive />
+			</div>
 
-							<div className="px-4 lg:px-6">
-								<ChartAreaInteractive />
-							</div>
-
-							<DataTable data={data} />
-						</div>
-					</div>
-				</div>
-			</SidebarInset>
-		</SidebarProvider>
+			<DataTable data={data} />
+		</>
 	);
 }
