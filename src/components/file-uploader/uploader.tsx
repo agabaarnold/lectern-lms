16071eb1f8325ce8d2d6145e6e45747c7210aa67
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-20 11:45:01 +0300

    feat(ui): implement Uploader component for file uploads with drag-and-drop support

---
+++ src/components/file-uploader/uploader.tsx
@@ -0,0 +1,195 @@
+// oxlint-disable shadcn/no-restyle
+import { cn } from "cn";
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import type { FileRejection } from "react-dropzone";
+import { toast } from "react-hot-toast";
+import { v4 as uuidv4 } from "uuid";
+
+import { Card, CardContent } from "../ui/card";
+import { Input } from "../ui/input";
+import { RenderEmptyState, RenderErrorState } from "./render-state";
+
+interface UploaderState {
+	id: string | null;
+	file: File | null;
+	uploading: boolean;
+	progress: number;
+	key?: string;
+	isDelecting: boolean;
+	error: boolean;
+	objectUrl?: string;
+	fileType: "image" | "video";
+}
+
+const rejectedFiles = (fileRejection: FileRejection[]) => {
+	if (fileRejection.length) {
+		const tooManyFiles = fileRejection.find(
+			(rejection) => rejection.errors[0].code === "too-many-files"
+		);
+
+		if (tooManyFiles) {
+			return toast.error("Too many files selected, max is 1");
+		}
+
+		const fileSizeTooBig = fileRejection.find(
+			(rejection) => rejection.errors[0].code === "file-too-large"
+		);
+
+		if (fileSizeTooBig) {
+			return toast.error("File size exceeds the limit");
+		}
+	}
+};
+
+export const Uploader = () => {
+	const [fileState, setFileState] = useState<UploaderState>({
+		error: false,
+		file: null,
+		id: null,
+		uploading: false,
+		progress: 0,
+		isDelecting: false,
+		fileType: "image",
+	});
+
+	const uploadFile = async (file: File) => {
+		setFileState((prev) => ({ ...prev, uploading: true, progress: 0 }));
+
+		try {
+			// 1. Get presigned URL
+			const presignedResponse = await fetch("/api/s3/upload", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					fileName: file.name,
+					contentType: file.type,
+					size: file.size,
+					isImage: true,
+				}),
+			});
+
+			if (!presignedResponse.ok) {
+				toast.error("Failed to get presigned URL");
+				return setFileState((prev) => ({
+					...prev,
+					uploading: false,
+					progress: 0,
+					error: true,
+				}));
+			}
+
+			const { presignedUrl, key } = await presignedResponse.json();
+
+			await new Promise<void>((resolve, reject) => {
+				const xhr = new XMLHttpRequest();
+
+				xhr.upload.onprogress = (e) => {
+					if (e.lengthComputable) {
+						const percentageCompleted = (e.loaded / e.total) * 100;
+
+						setFileState((prev) => ({
+							...prev,
+							progress: Math.round(percentageCompleted),
+						}));
+					}
+				};
+
+				xhr.onload = () => {
+					if (xhr.status === 200 || xhr.status === 204) {
+						setFileState((prev) => ({
+							...prev,
+							uploading: false,
+							progress: 100,
+							key,
+						}));
+
+						toast.success("File uploaded successfully");
+						resolve();
+					} else {
+						reject(new Error("Upload failed..."));
+					}
+
+					xhr.onerror = () => {
+						reject(new Error("Upload failed"));
+					};
+
+					xhr.open("PUT", presignedUrl);
+					xhr.setRequestHeader("Content-Type", file.type);
+					xhr.send(file);
+				};
+			});
+		} catch {
+			toast.error("Something went wrong");
+
+			setFileState((prev) => ({
+				...prev,
+				progress: 0,
+				error: true,
+				uploading: false,
+			}));
+		}
+	};
+
+	const onDrop = useCallback((acceptedFiles: File[]) => {
+		if (acceptedFiles.length > 0) {
+			const file = acceptedFiles[0];
+
+			setFileState({
+				file,
+				uploading: false,
+				progress: 0,
+				objectUrl: URL.createObjectURL(file),
+				error: false,
+				id: uuidv4(),
+				isDelecting: false,
+				fileType: "image",
+			});
+
+			uploadFile(file);
+		}
+	}, []);
+
+	const { getInputProps, getRootProps, isDragActive } = useDropzone({
+		onDrop,
+		accept: { "image/*": [] },
+		maxFiles: 1,
+		multiple: false,
+		maxSize: 5 * 1024 * 1024,
+		onDropRejected: rejectedFiles,
+	});
+
+	const renderContent = () => {
+		if (fileState.uploading) {
+			return <h1>Uploading...</h1>;
+		}
+
+		if (fileState.error) {
+			return <RenderErrorState />;
+		}
+
+		if (fileState.objectUrl) {
+			return <h1>Uploaded file</h1>;
+		}
+
+		<RenderEmptyState isDragActive={isDragActive} />;
+	};
+
+	return (
+		<Card
+			className={cn(
+				"relative h-64 w-full border-2 border-dashed transition-colors duration-200",
+				isDragActive
+					? "border-primary bg-primary/10 border-solid"
+					: "border-border hover:border-primary"
+			)}
+			{...getRootProps()}
+		>
+			<CardContent className="flex h-full w-full items-center justify-center p-4">
+				<Input {...getInputProps()} />
+
+				{renderContent()}
+			</CardContent>
+		</Card>
+	);
+};
