3d78343fae960e1f49486836b2e9bf64c28622ec
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-20 11:44:10 +0300

    feat(ui): add render states for file uploader component

---
+++ src/components/file-uploader/render-state.tsx
@@ -0,0 +1,46 @@
+import { IconCloudUpload, IconPhoto } from "@tabler/icons-react";
+import { cn } from "cn";
+
+import { Button } from "../ui/button";
+
+// TODO: Use the Empty ui component if applicable
+export const RenderEmptyState = ({
+	isDragActive,
+}: {
+	isDragActive: boolean;
+}) => (
+	<div className="text-center">
+		<div className="bg-muted mx-auto mb-4 flex size-12 items-center justify-center rounded-full">
+			<IconCloudUpload
+				className={cn(
+					"text-muted-foreground size-6",
+					isDragActive && "text-primary"
+				)}
+			/>
+		</div>
+
+		<p className="text-foreground text-base font-semibold">
+			Drop your files here or{" "}
+			<span className="text-primary cursor-pointer font-bold">
+				click to upload
+			</span>
+		</p>
+
+		<Button className="mt-4" type="button">
+			Select a file
+		</Button>
+	</div>
+);
+
+export const RenderErrorState = () => (
+	<div className="text-center">
+		<div className="bg-destructive/30 mx-auto mb-4 flex size-12 items-center justify-center rounded-full">
+			<IconPhoto className="text-destructive size-6" />
+		</div>
+
+		<p className="text-base font-semibold">Upload failed</p>
+		<p className="text-muted-foreground mt-1 text-xs">Something went wrong</p>
+        
+		<Button className="mt-4">Retry selecting a file</Button>
+	</div>
+);
