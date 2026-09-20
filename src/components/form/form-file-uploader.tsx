d62f3a77c466375dba94f08b3830a42fb4320a4a
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-20 14:04:47 +0300

    fix(form-file-uploader): enhance validation handling and improve field structure

--- src/components/form/form-file-uploader.tsx
+++ src/components/form/form-file-uploader.tsx
@@ -1,20 +1,27 @@
 import { useFieldContext } from "#/hooks/form/use-form-context.ts";
 
 import { Uploader } from "../file-uploader/uploader";
-import { Field, FieldLabel } from "../ui/field";
+import { Field, FieldError, FieldLabel } from "../ui/field";
 
 interface FormFileUploaderProps {
 	label: string;
 }
 
 const FormFileUploader = ({ label }: FormFileUploaderProps) => {
-	const field = useFieldContext();
+	const field = useFieldContext<string>();
+	const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
 
 	return (
-		<Field>
-			<FieldLabel>{label}</FieldLabel>
-            
-			<Uploader />
+		<Field data-invalid={isInvalid}>
+			<FieldLabel htmlFor={field.name}>{label}</FieldLabel>
+
+			<Uploader
+				onBlur={() => field.handleBlur()}
+				onValueChange={(key) => field.handleChange(key)}
+				value={field.state.value}
+			/>
+
+			{isInvalid && <FieldError errors={field.state.meta.errors} />}
 		</Field>
 	);
 };
