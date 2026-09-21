5aec776f27bbb569efa94f966b45c171e3de6305
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-21 13:05:54 +0300

    feat(migration): add user banning functionality and impersonation tracking

---
+++ drizzle/20260921100407_many_vulcan/migration.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "sessions" ADD COLUMN "impersonated_by" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "banned" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "ban_reason" text;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "ban_expires" timestamp;--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "role" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "role" DROP NOT NULL;
