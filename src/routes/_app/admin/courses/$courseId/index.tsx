16392b6a72c96c572d2a22d54aaa56205cdf663d
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-21 17:25:02 +0300

    feat(course-route): add route for course details page with placeholder component

---
+++ src/routes/_app/admin/courses/$courseId/index.tsx
@@ -0,0 +1,10 @@
+// oxlint-disable react/function-component-definition func-style
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_app/admin/courses/$courseId/")({
+	component: RouteComponent,
+});
+
+function RouteComponent() {
+	return <div>Hello "/_app/admin/courses/$courseId"!</div>;
+}
