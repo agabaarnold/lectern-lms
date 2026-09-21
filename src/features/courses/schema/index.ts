fa8bd3522f25fb371db826b690bedfd1673eb18d
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-21 17:24:31 +0300

    feat(course): extend updateCourseSchema to include course ID and require at least one field for updates

--- src/features/courses/schema/index.ts
+++ src/features/courses/schema/index.ts
@@ -63,6 +63,7 @@ export type GetCoursesQuery = z.input<typeof getCoursesQuerySchema>;
 
 export const updateCourseSchema = courseSchema
 	.partial()
+	.extend({ id: z.uuid() })
 	.refine((value) => Object.keys(value).length > 1, {
 		error: "Provide at least one field to update",
 	});
