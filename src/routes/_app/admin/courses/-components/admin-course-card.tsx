e97b1693b8b039a92f2e26088c5128870104d58e
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-22 08:41:10 +0300

    refactor(admin-course-card): remove unused DropdownMenuGroup import

--- src/routes/_app/admin/courses/-components/admin-course-card.tsx
+++ src/routes/_app/admin/courses/-components/admin-course-card.tsx
@@ -16,7 +16,6 @@ import { Card, CardContent } from "#/components/ui/card.tsx";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
-	DropdownMenuGroup,
 	DropdownMenuItem,
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
