04d64d5a283597783d36be3582365e3b39458aa7
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-23 13:38:59 +0300

    feat(LessonPage): implement lesson loading and display in the lesson details page

--- src/routes/_app/admin/courses/$courseId/$chapterId/$lessonId/index.tsx
+++ src/routes/_app/admin/courses/$courseId/$chapterId/$lessonId/index.tsx
@@ -1,14 +1,28 @@
-// oxlint-disable react/function-component-definition func-style
 import { createFileRoute } from "@tanstack/react-router";
+
+// oxlint-disable react/function-component-definition func-style
+import { getLesson } from "#/features/courses/functions/lessons.ts";
+
+import { LessonForm } from "./-components/lesson-form";
 
 export const Route = createFileRoute(
 	"/_app/admin/courses/$courseId/$chapterId/$lessonId/"
 )({
+	loader: async ({ params }) => {
+		const { lessonId } = params;
+
+		const lesson = await getLesson({ data: { id: lessonId } });
+
+		return { lesson };
+	},
 	component: LessonPage,
 });
 
 function LessonPage() {
+	const { lesson } = Route.useLoaderData();
+	const { chapterId, courseId } = Route.useParams();
+
 	return (
-		<div>Hello "/_app/admin/courses/$courseId/$chapterId/$lessonId/"!</div>
+		<LessonForm lesson={lesson} chapterId={chapterId} courseId={courseId} />
 	);
 }
