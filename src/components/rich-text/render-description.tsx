fac3161f5d49d87399373bd9a7990ab4f9de35c4
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-23 22:55:30 +0300

    feat(render-description): add RenderDescription component for rendering rich text

---
+++ src/components/rich-text/render-description.tsx
@@ -0,0 +1,23 @@
+import { TextAlign } from "@tiptap/extension-text-align";
+import { generateHTML } from "@tiptap/react";
+import type { JSONContent } from "@tiptap/react";
+import { StarterKit } from "@tiptap/starter-kit";
+import parse from "html-react-parser";
+import { useMemo } from "react";
+
+export const RenderDescription = ({ json }: { json: JSONContent }) => {
+	const output = useMemo(
+		() =>
+			generateHTML(json, [
+				StarterKit,
+				TextAlign.configure({ types: ["heading", "paragraph"] }),
+			]),
+		[json]
+	);
+
+	return (
+		<div className="prose dark:prose-invert prose-li:marker:text-primary">
+			{parse(output)}
+		</div>
+	);
+};
