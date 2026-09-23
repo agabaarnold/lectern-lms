7d1a345a976c05efebbb0965a6a4fef896b0ca15
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-23 17:45:34 +0300

    fix(PublicLayout): add bottom margin to main container for improved spacing

--- src/routes/_public/route.tsx
+++ src/routes/_public/route.tsx
@@ -12,7 +12,7 @@ function PublicLayout() {
 		<div className="">
 			<Navbar />
 
-			<main className="container mx-auto px-4 md:px-6 lg:px-8">
+			<main className="container mx-auto px-4 md:px-6 lg:px-8 mb-32">
 				<Outlet />
 			</main>
 		</div>
