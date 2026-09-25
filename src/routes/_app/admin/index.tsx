a8e2e853880b64210ddd1454aa0e51e6492c9206
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-25 11:08:52 +0300

    refactor: remove DataTable component and related imports from admin dashboard

--- src/routes/_app/admin/index.tsx
+++ src/routes/_app/admin/index.tsx
@@ -1,9 +1,7 @@
 // oxlint-disable react/function-component-definition func-style
 import { createFileRoute } from "@tanstack/react-router";
 
-import data from "#/app/dashboard/data.json";
 import { ChartAreaInteractive } from "#/components/sidebar/chart-area-interactive.tsx";
-import { DataTable } from "#/components/sidebar/data-table.tsx";
 import { SectionCards } from "#/components/sidebar/section-cards.tsx";
 
 export const Route = createFileRoute("/_app/admin/")({
@@ -18,8 +16,6 @@ function AdminDashboard() {
 			<div className="px-4 lg:px-6">
 				<ChartAreaInteractive />
 			</div>
-
-			<DataTable data={data} />
 		</>
 	);
 }
