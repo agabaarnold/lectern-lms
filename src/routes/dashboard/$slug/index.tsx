015a86f87a84bbb9c6af9605324a2595b5fb2df4
Agaba Arnold <agabaarnold23@gmail.com>
2026-09-26 15:52:18 +0300

    feat(dashboard): implement lesson redirection and update no lessons message

--- src/routes/dashboard/$slug/index.tsx
+++ src/routes/dashboard/$slug/index.tsx
@@ -1,10 +1,34 @@
 // oxlint-disable react/function-component-definition func-style
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+import { getCourseSiderbarData } from "#/features/courses/functions/courses.ts";
 
 export const Route = createFileRoute("/dashboard/$slug/")({
+	loader: async ({ params }) => {
+		const data = await getCourseSiderbarData({
+			data: { slug: params.slug },
+		});
+
+		const firstLesson = data.course.chapters[0]?.lessons[0];
+
+		if (firstLesson) {
+			throw redirect({
+				to: "/dashboard/$slug/$lessonId",
+				params: { slug: params.slug, lessonId: firstLesson.id },
+			});
+		}
+
+		return data;
+	},
 	component: CourseSlugRoute,
 });
 
 function CourseSlugRoute() {
-	return <div>Hello "/dashboard/$slug/"!</div>;
+	return (
+		<div className="flex items-center justify-center">
+			<h2 className="mb-2 text-2xl font-bold">No lessons available</h2>
+
+			<p>This course does not have any lessons yet</p>
+		</div>
+	);
 }
